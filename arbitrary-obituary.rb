f5f7a99fb596a2a8af8927136c47851d0410f2a5
Josh Baker <jtbaker@jtbaker.name>
2016-10-05 17:35:37 -0700

    Fix the middle initial function

--- arbitrary-obituary.rb
+++ arbitrary-obituary.rb
@@ -140,8 +140,7 @@ class Obituary
 		@middleInitial ||= CSV::read("names/first/yob#{birthDate.year}.txt")
 		.map{ |row| {:name => row[0], :gender => (row[1] == 'F' ? 'female' : 'male'), :count => row[2].to_i} }
 		.select{ |row| row[:gender].to_s == gender.to_s }
-		.tap{ |n| puts "Reducing #{n.length} rows like #{n.first} or #{n[1]} into initials" }
-		.reduce(Hash.new(0)){ |m, row| m[row[:name].first] += row[:count] }
+		.reduce(Hash.new(0)){ |m, row| m[row[:name][0]] += row[:count]; m }
 		.weighted_sample
 	end
 	
