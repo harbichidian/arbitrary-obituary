2c25b448e28d203cd3f8c74968d105fde4241dd5
Josh Baker <jtbaker@jtbaker.name>
2016-10-16 12:11:43 -0700

    Complete the outputs

--- arbitrary-obituary.rb
+++ arbitrary-obituary.rb
@@ -29,13 +29,13 @@ class Obituary
 	
 	def announcementTemplate
 		@announcementTemplate ||= [
-			"#{firstName} #{middleInitial ? middleInitial + " " : ""}#{lastName} (#{birthDate} - #{deathDate})"
+			"#{firstName} #{middleInitial ? middleInitial + " " : ""}#{lastName} (#{birthDate.year} - #{deathDate.year})"
 		].sample
 	end
 	
 	def birthTemplate
 		@birthTemplate ||= [
-			"#{pronounSubject.capitalize} was born #{birthDate} in #{birthLocation} to #{fatherFirstName} #{fatherLastName} and #{motherFirstName} #{motherLastName}. #{pronounSubject.capitalize} attended #{highSchool}"
+			"#{pronounSubject.capitalize} was born #{birthDate.strftime("%B %-d, %Y")} in #{birthLocation} to #{fatherFirstName} #{fatherLastName} and #{motherFirstName} #{motherLastName}. #{pronounSubject.capitalize} attended #{highSchool}."
 		].sample
 	end
 	
@@ -85,8 +85,22 @@ class Obituary
 		].sample
 	end
 	
+	def fatherFirstName
+		# TODO: Make sure my list of names actually covers the oldest possible year.
+		# TODO: Dynamically determine father's birth date based on parental age data.
+		@fatherFirstName ||= firstName!(birthDate: birthDate - Array(20..40).sample.years, gender: "male")
+	end
+	
+	def fatherLastName
+		@fatherLastName ||= lastName!
+	end
+	
 	def firstName
-		@firstName ||= CSV::read("names/first/yob#{birthDate.year}.txt")
+		@firstName ||= firstName!
+	end
+	
+	def firstName!(birthDate: self.birthDate, gender: self.gender)
+		CSV::read("names/first/yob#{birthDate.year}.txt")
 		.map{ |row| {:name => row[0], :gender => (row[1] == 'F' ? 'female' : 'male'), :count => row[2].to_i} }
 		.select{ |row| row[:gender].to_s == gender.to_s }
 		.map{ |row| [row[:name], row[:count]] }
@@ -101,8 +115,16 @@ class Obituary
 		}.weighted_sample
 	end
 	
+	def highSchool
+		@highSchool ||= "#{location!} High School"
+	end
+	
 	def lastName
-		@lastName ||= CSV::read("names/last.txt")
+		@lastName ||= lastName!
+	end
+	
+	def lastName!
+		CSV::read("names/last.txt")
 		.map{ |row| [row[0].capitalize, row[1].to_i] }
 		.to_h
 		.weighted_sample
@@ -150,6 +172,15 @@ class Obituary
 		.weighted_sample
 	end
 	
+	def motherFirstName
+		@motherFirstName ||= firstName!(birthDate: birthDate - Array(20..40).sample.years, gender: "female")
+	end
+	
+	def motherLastName
+		# TODO: Determine the actual likelihood that the mother does not have the same last name as the father.
+		@motherLastName ||= (Random.rand > 0.2 ? fatherLastName : lastName!)
+	end
+	
 	def preposition
 		@preposition ||= %w[
 			from
@@ -159,25 +190,25 @@ class Obituary
 	
 	def pronounObject
 		case gender
-		when "male" then "him"
-		when "female" then "her"
-		when "neuter" then "them"
+		when "male" then return "him"
+		when "female" then return "her"
+		when "neuter" then return "them"
 		end
 	end
 	
 	def pronounPossessive
 		case gender
-		when "male" then "his"
-		when "female" then "her"
-		when "neuter" then "their"
+		when "male" then return "his"
+		when "female" then return "her"
+		when "neuter" then return "their"
 		end
 	end
 	
 	def pronounSubject
 		case gender
-		when "male" then "he"
-		when "female" then "she"
-		when "neuter" then "they"
+		when "male" then return "he"
+		when "female" then return "she"
+		when "neuter" then return "they"
 		end
 	end
 end
